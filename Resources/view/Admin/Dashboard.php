daaee85d96c031cc15db8545c7f8897f9b0cc8ab
imon213 <emonf666@gmail.com>
2022-12-07 22:53:21 +0600

    admin dashboard design

---
+++ Resources/view/Admin/Dashboard.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/University-Management/Resources/css/Admin/Dashboard.css">
+    <title>Dashboard</title>
+</head>
+
+<body>
+    <div class="container">
+        <!-- top-nav started -->
+        <div class="top-nav">
+            <div class="left">
+                <h3>LOGO</h3>
+            </div>
+            <div class="right">
+                <ul>
+                    <li>Home</li>
+                    <li>Logout</li>
+                </ul>
+            </div>
+        </div>
+        <!-- top-nav ended -->
+        <!-- main-content started -->
+        <div class="main-content">
+            <div class="left-main">
+                <ul>
+                    <li><a href="#">ADD FACULTY</a> </li>
+                    <li><a href="#">ADD STUDENT</a> </li>
+                    <li><a href="#">FACULTY LIST</a> </li>
+                    <li><a href="#">STUDENT LIST</a> </li>
+                    <li><a href="#">STUDENT RESISTRATION</a> </li>
+                </ul>
+            </div>
+            <div class="right-main">
+               <h3>Hello Admin. Hope so you will have a good time with us..</h3>
+            </div>
+        </div>
+        <!-- main-content ended -->
+    </div>
+</body>
+
+</html>
